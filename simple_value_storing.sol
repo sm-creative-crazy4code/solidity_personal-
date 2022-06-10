3a7d8ded0189757c95b8e9405a6ed73b36ae2917
sneha_mandal <66171774+sm-creative-crazy4code@users.noreply.github.com>
2022-06-10 19:58:20 +0530

    Create simple_value_storing.sol

---
+++ simple_value_storing.sol
@@ -0,0 +1,100 @@
+/////GIVING THE LICENSE NUMBER DEFINE THE VERSIONTHT THE CONTRACT IS COMPATIBLE WITH
+
+ pragma solidity >=0.5.0<0.9.0;
+ //next define the contract
+ contract storeValue{
+
+address valueAddress;
+uint value;
+
+//function to set value
+//pass value to the given function
+//specify visibility of the function or it will give errors
+function set(uint x) public {
+    value=x;
+
+
+
+}
+//function to getvalue
+/////GIVING THE LICENSE NUMBER DEFINE THE VERSIONTHT THE CONTRACT IS COMPATIBLE WITH
+
+ pragma solidity >=0.5.0<0.9.0;
+ //next define the contract
+ contract storeValue{
+
+address valueAddress;
+uint value;
+
+//function to set value
+//pass value to the given function
+//specify visibility of the function or it will give errors
+function set(uint x) public {
+    value=x;
+
+
+
+}
+//function to getvalue
+//get function takes no parametesrs but returns the value
+//since this function is not doing any operation its state can be restricted to view
+function get()  public view  returns (uint){
+    return value;
+
+
+}
+//contract  architecture
+//define license--> define version-->name a contract-->initialize value-->setvalue-->get value
+//deploy button deploys the contractFOR USER TO INTERACT WITH
+
+
+
+
+ }get function takes no parametesrs but returns the value
+//since this function is not doing any operation its state can be restricted to view
+function get()  public view  returns (uint){
+    return value;
+
+
+}
+//contract  architecture
+//define license--> define version-->name a contract-->initialize value-->setvalue-->get value
+//deploy button deploys the contractFOR USER TO INTERACT WITH
+
+
+
+
+ }/////GIVING THE LICENSE NUMBER DEFINE THE VERSIONTHT THE CONTRACT IS COMPATIBLE WITH
+
+ pragma solidity >=0.5.0<0.9.0;
+ //next define the contract
+ contract storeValue{
+
+address valueAddress;
+uint value;
+
+//function to set value
+//pass value to the given function
+//specify visibility of the function or it will give errors
+function set(uint x) public {
+    value=x;
+
+
+
+}
+//function to getvalue
+//get function takes no parametesrs but returns the value
+//since this function is not doing any operation its state can be restricted to view
+function get()  public view  returns (uint){
+    return value;
+
+
+}
+//contract  architecture
+//define license--> define version-->name a contract-->initialize value-->setvalue-->get value
+//deploy button deploys the contractFOR USER TO INTERACT WITH
+
+
+
+
+ }
