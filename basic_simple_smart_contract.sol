8274ccc0dfee4fedc5c6d348529d9752c74ee109
sneha_mandal <66171774+sm-creative-crazy4code@users.noreply.github.com>
2022-06-10 23:22:46 +0530

    Create basic_simple_smart_contract.sol

---
+++ basic_simple_smart_contract.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.0<0.9.0;
+contract SimpleSmartComtract{
+    //we need to specify the return type and visibility oft the function
+
+function hello() public pure returns (string memory){
+    return 'hello wiorld';
+
+}
+//pure functioon does not reads or write any data from the block chain
+// remix analyses a smart contract and creates a button for it 
+}
+//contracts are like objects in js
+    //in a single solidity file one can have several smart contract
+  //  contract simple2{}
+  //each smart contract intrest has its owm addressmon block chain
+
+    
